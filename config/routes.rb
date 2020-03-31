8368a65ea17e07c67916802f533975da691c1d3a
Kelly Sandeson <kellysandeson@gmail.com>
2020-03-30 18:05:09 -0600

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#show'
+
+  get '/reservations', to: 'reservations#index'
+  get '/expenses', to: 'expenses#index'
+  get '/leases', to: 'leases#index'
+
 end
