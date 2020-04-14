f5b81051c8ab6a276a29793773ce1798d9117cb5
Kelly Sandeson <kellysandeson@gmail.com>
2020-04-13 18:12:14 -0600

    add a show route for the leases

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#show'
 
   get '/reservations', to: 'reservations#index'
+
   get '/expenses', to: 'expenses#index'
+
   get '/leases', to: 'leases#index'
+  get '/leases/:id', to: 'leases#show'
 
 end
