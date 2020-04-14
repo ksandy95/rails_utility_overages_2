e8ee8e12cfc9c203f716dac8742269637c4bd4fb
Kelly Sandeson <kellysandeson@gmail.com>
2020-04-14 16:24:01 -0600

    Playing around with Faker

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -6,6 +6,8 @@ class LeasesController < ApplicationController
 
   def show
     @lease = Lease.where(unit_id: params[:id])
+    # @address = {city: Faker::Address.city, street_name: Faker::Address.street_name, street_address: Faker::Address.street_address, unit: Faker::Address.secondary_address, zip: Faker::Address.zip, state: Faker::Address.state}
+    # @fake_address = FactoryGirl.build(:destination)
   end
 
 end
