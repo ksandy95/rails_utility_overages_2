b985e6c727afcb768de36c3121a3ca2f73ecb7a0
Kelly Sandeson <kellysandeson@gmail.com>
2020-04-14 17:50:42 -0600

    Make place holders for Controller

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -8,6 +8,14 @@ class LeasesController < ApplicationController
     @lease = Lease.where(unit_id: params[:id])
     # @address = {city: Faker::Address.city, street_name: Faker::Address.street_name, street_address: Faker::Address.street_address, unit: Faker::Address.secondary_address, zip: Faker::Address.zip, state: Faker::Address.state}
     # @fake_address = FactoryGirl.build(:destination)
+
+    @last_month_expense_sum = 0
+    @last_month_expenses = []
+    @last_month_reservations = []
+
+    @current_month_expense_sum = 0
+    @current_month_expenses = []
+    @current_month_reservations = []
   end
 
 end
