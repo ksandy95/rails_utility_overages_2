38ebdeb8e334471199da8f6eee43131445b83e5e
Kelly Sandeson <kellysandeson@gmail.com>
2020-04-14 19:13:40 -0600

    Make some fake reservation numbers

--- app/controllers/leases_controller.rb
+++ app/controllers/leases_controller.rb
@@ -6,8 +6,10 @@ class LeasesController < ApplicationController
 
   def show
     @lease = Lease.where(unit_id: params[:id])
+
     # @address = {city: Faker::Address.city, street_name: Faker::Address.street_name, street_address: Faker::Address.street_address, unit: Faker::Address.secondary_address, zip: Faker::Address.zip, state: Faker::Address.state}
     # @fake_address = FactoryGirl.build(:destination)
+
     expenses = Expense.first(6)
     @last_month_expenses = [expenses[0], expenses[1], expenses[2]]
     x = []
@@ -15,15 +17,23 @@ class LeasesController < ApplicationController
       x << expense.net
     end
     @last_month_expense_sum = x.sum
-    @last_month_reservations = []
+
+    res1 = rand(11111..99999)
+    res2 = rand(11111..99999)
+    @last_month_reservations = [res1, res2]
+
+
 # ------------------------------------------------------------------------
+
     @current_month_expenses = [expenses[3], expenses[4], expenses[5]]
     y = []
     @current_month_expenses.each do |expense|
       y << expense.net
     end
     @current_month_expense_sum = y.sum
-    @current_month_reservations = []
+
+    res3 = rand(11111..99999)
+    @current_month_reservations = [res2, res3]
   end
 
 end
