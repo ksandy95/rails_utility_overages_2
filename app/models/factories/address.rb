f6a78330163580298989314ad544bb4a1f2e5946
Kelly Sandeson <kellysandeson@gmail.com>
2020-04-15 14:30:10 -0600

    Delete facotry

--- app/models/factories/address.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :address do
-    city           { Faker::Address.city }
-    street_address { Faker::Address.street_address }
-    unit           { Faker::Address.secondary_address }
-    zip            { Faker::Address.zip }
-    state          { Faker::Address.state }
-  end
-end
